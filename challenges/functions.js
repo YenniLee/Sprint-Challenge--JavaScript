a360d9d8aa60a5786eee1e69d9109b0742d7fd62
Jennie Van <missjenvan@gmail.com>
2019-08-23 09:20:56 -0700

    mvp for functions assignment

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,32 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, cb){
+  return cb(a, b);
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1, num2){
+  return num1 + num2;
+};
 
+function multiply(num1, num2){
+  return num1 * num2;
+};
+
+function greeting(first_name, last_name){
+  return `Hello ${first_name} ${last_name}, nice to meet you!`;
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,6 +40,7 @@
 
 // Explanation: 
 
+//closures are created everytime a function is declared and gives access to variables at different scopes around it. nestedFunctiton has access to to the internal variable on line 49 because it is nested in the scope of the larger myFunction().
 
 const external = "I'm outside the function";
 
